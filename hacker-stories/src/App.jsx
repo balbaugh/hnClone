afcdb9964242f98b79767bce4ea180fe8550421b
balbaugh <balbaugh@gmail.com>
2023-05-08 23:36:17 +0300

    exercise progress

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -82,17 +82,19 @@ const App = () => {
 		'React'
 	);
 
+	const [url, setUrl] = React.useState(
+		`${API_ENDPOINT}${searchTerm}`
+	);
+
 	const [stories, dispatchStories] = React.useReducer(
 		storiesReducer,
 		{ data: [], isLoading: false, isError: false }
 	);
 
-	React.useEffect(() => {
-		if (!searchTerm) return;
-
+	const handleFetchStories = React.useCallback(() => {
 		dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-		fetch(`${API_ENDPOINT}${searchTerm}`)
+		fetch(url)
 			.then((response) => response.json())
 			.then((result) => {
 				dispatchStories({
@@ -103,7 +105,11 @@ const App = () => {
 			.catch(() =>
 				dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
 			);
-	}, [searchTerm]);
+	}, [url]);
+
+	React.useEffect(() => {
+		handleFetchStories();
+	}, [handleFetchStories]);
 
 	const handleRemoveStory = (item) => {
 		dispatchStories({
@@ -112,8 +118,12 @@ const App = () => {
 		});
 	};
 
-	const handleSearch = (event) => {
+	const handleSearchInput = (event) => {
 		setSearchTerm(event.target.value);
+	};
+
+	const handleSearchSubmit = () => {
+		setUrl(`${API_ENDPOINT}${searchTerm}`);
 	};
 
 	return (
@@ -124,10 +134,18 @@ const App = () => {
 				id='search'
 				value={searchTerm}
 				isFocused
-				onInputChange={handleSearch}
+				onInputChange={handleSearchInput}
 			>
 				<strong>Search:</strong>
 			</InputWithLabel>
+
+			<button
+				type='button'
+				disabled={!searchTerm}
+				onClick={handleSearchSubmit}
+			>
+				Submit
+			</button>
 
 			<hr />
 
