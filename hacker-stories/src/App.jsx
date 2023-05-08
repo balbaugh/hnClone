e6a6b1dd9df42188d2068e116f522835d0eab62f
balbaugh <balbaugh@gmail.com>
2023-05-08 19:00:36 +0300

    exercise progress

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,25 +1,6 @@
 import * as React from 'react';
 import './App.css';
 
-const list = [
-	{
-		title: 'React',
-		url: 'https://reactjs.org/',
-		author: 'Jordan Walke',
-		num_comments: 3,
-		points: 4,
-		objectID: 0,
-	},
-	{
-		title: 'Redux',
-		url: 'https://redux.js.org/',
-		author: 'Dan Abramov, Andrew Clark',
-		num_comments: 2,
-		points: 5,
-		objectID: 1,
-	},
-];
-
 const App = () => {
 	const stories = [
 		{
@@ -40,48 +21,52 @@ const App = () => {
 		},
 	];
 
+	const [searchTerm, setSearchTerm] = React.useState('React');
+
+	const handleSearch = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
+	const searchedStories = stories.filter((story) =>
+		story.title.toLowerCase().includes(searchTerm.toLowerCase())
+	);
+
 	return (
 		<div>
 			<h1>My Hacker Stories</h1>
 
-			<Search />
+			<Search search={searchTerm} onSearch={handleSearch} />
 
 			<hr />
 
-			<List list={stories} />
+			<List list={searchedStories} />
 		</div>
 	);
 };
 
-const Search = () => {
-	const handleChange = (event) => {
-		// synthetic event
-		console.log(event);
-		// value of target (here: input HTML element)
-		console.log(event.target.value);
-	};
+const Search = ({ search, onSearch }) => (
+	<div>
+		<label htmlFor="search">Search: </label>
+		<input id="search" type="text" value={search} onChange={onSearch} />
+	</div>
+);
 
-	return (
-		<div>
-			<label htmlFor="search">Search: </label>
-			<input id="search" type="text" onChange={handleChange} />
-		</div>
-	);
-};
-
-const List = () => (
+const List = ({ list }) => (
 	<ul>
 		{list.map((item) => (
-			<li key={item.objectID}>
-				<span>
-					<a href={item.url}>{item.title}</a>
-				</span>
-				<span>{item.author}</span>
-				<span>{item.num_comments}</span>
-				<span>{item.points}</span>
-			</li>
+			<Item key={item.objectID} item={item} />
 		))}
 	</ul>
+);
+const Item = ({ item }) => (
+	<li>
+		<span>
+			<a href={item.url}>{item.title}</a>
+		</span>
+		<span>{item.author}</span>
+		<span>{item.num_comments}</span>
+		<span>{item.points}</span>
+	</li>
 );
 
 export default App;
